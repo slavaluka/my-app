ebe95e4d03c6062bc7b55ac0d5d082a4b35e2855
Sláva Lukianchuk <106429742+slavaluka@users.noreply.github.com>
2023-06-09 00:39:52 +0300

    New Auth

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const index = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
+
+export default index;
